ec387bccfc35b2fcc365e6e07b0d3069587de6c1
user17872 <114112501+user17872@users.noreply.github.com>
2022-11-29 16:15:24 -0800

    Updated the game
    Updated app.js

--- src/app.js
+++ src/app.js
@@ -1,14 +1,33 @@
+//Player x
 let playerx = 'x';
 let playerxcount = 0;
 
+//Player o
 let playero = 'o';
 let playerocount = 0;
 
+//The turn and board
 let turn = 0;
 let board = 0;
-let gameBoard = [0, 0, 0, 0, 0, 0, 0, 0, 0];
 
-function fillin(eachmove) {
+//the 3x3 grid and each possible winning combination
+let gameBoard = [	0, 0, 0, 
+					0, 0, 0, 
+					0, 0, 0];
+const combination = [
+	[0, 3, 6],
+	[1, 4, 7],
+	[2, 5, 8],
+	[0, 1, 2],
+	[3, 4, 5],
+	[6, 7, 8],
+	[0, 4, 8],
+	[2, 4, 6],
+];
+
+
+//Takes in 
+function populate(i) {
 	let moveturn = document.createElement('div');
 	moveturn.className =
 		'border-2 border-black cursor-pointer flex items-center justify-center h-full w-full text-5xl moveturn-class';
@@ -17,12 +36,12 @@ function fillin(eachmove) {
 		(event) => {
 			const { target } = event;
 			target.textContent = turn;
-			gameBoard[eachmove] = turn;
-			checkBoard();
+			gameBoard[i] = turn;
+			point();
 
-			if (turn === playero) {
+			if(turn === playero) {
 				turn = playerx;
-			} else {
+			}else {
 				turn = playero;
 			}
 		},
@@ -31,30 +50,16 @@ function fillin(eachmove) {
 	return moveturn;
 }
 
-function checkBoard() {
-	const combination = [
-		[0, 3, 6],
-		[1, 4, 7],
-		[2, 5, 8],
-		[0, 1, 2],
-		[3, 4, 5],
-		[6, 7, 8],
-		[0, 4, 8],
-		[2, 4, 6],
-	];
-
+function point() {
 	for (let combo of combination) {
-		let [combo1, combo2, combo3] = combo;
 		if (
-			gameBoard[combo1] !== '' &&
-			gameBoard[combo1] === gameBoard[combo2] &&
-			gameBoard[combo1] === gameBoard[combo3]
+			(gameBoard[combo[0]] === gameBoard[combo[1]] && gameBoard[combo[1]] === gameBoard[combo[2]]) && (gameBoard[combo[0]] !== 0)
 		) {
 			document.getElementById('hid').innerHTML = `Player ${turn} won the game`;
-			if (turn === 'o') {
+			if(turn === 'o') {
 				playerocount += 1;
 				document.getElementById('play1').innerHTML = `${playerocount}`;
-			} else if (turn === 'x') {
+			}else if (turn === 'x') {
 				playerxcount += 1;
 				document.getElementById('play2').innerHTML = `${playerxcount}`;
 			}
@@ -62,17 +67,22 @@ function checkBoard() {
 	}
 }
 
+
 function start() {
+	
 	turn = playero;
 	board = document.getElementById('boardid');
 
 	for (let i = 0; i < 9; i++) {
-		board.appendChild(fillin(i));
+		board.appendChild(populate(i));
 	}
 
-	gameBoard.fill('');
+	gameBoard.fill(0);
 
 	document.body.appendChild(board);
+
+	
 }
+
 
 start();
